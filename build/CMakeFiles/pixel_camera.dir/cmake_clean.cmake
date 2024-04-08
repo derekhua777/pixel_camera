765a61d8b0400d7d4f88a75512eb22c7698ca7ab
Derek Hua <derekhua@ucla.edu>
2024-04-07 17:08:23 -0700

    opencv stream and pixelate

---
+++ build/CMakeFiles/pixel_camera.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pixel_camera.dir/main.cpp.o"
+  "CMakeFiles/pixel_camera.dir/main.cpp.o.d"
+  "pixel_camera"
+  "pixel_camera.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pixel_camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/pixel_camera.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pixel_camera.
+# This may be replaced when dependencies are built.
